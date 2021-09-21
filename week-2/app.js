e55783d2a3bfa81b45ad7abffd75fb81b5965415
Jeremy Odell <jeremyodell@jeremys-mbp.wireless.newschool.edu>
2021-09-21 15:08:14 -0400

    week3 stuff

--- week-2/app.js
+++ week-2/app.js
@@ -1,9 +1,10 @@
 var fs = require('fs');
 var cheerio = require('cheerio');
 
+const checkForZipcode = str => (/\b\d{5}\b/g).test(str)
+
 const parseFile = (fileName) => fs.readFile(fileName, 'utf8', function(err, data) {
   if (err) throw err;
-  console.log('OK: ' + fileName);
 
   var $ = cheerio.load(data);
 
@@ -13,23 +14,27 @@ const parseFile = (fileName) => fs.readFile(fileName, 'utf8', function(err, data
     .map(el => el.replace(/\t/g, ''))
     .map(el => el.replace(/\n/g, ''))
     .filter(string => string != '')
-    .filter((el, i, arr) => (/\b\d{5}\b/g).test(el) || (/\b\d{5}\b/g).test(arr[i + 1]))
+    .filter((el, i, arr) => checkForZipcode(el) || checkForZipcode(arr[i + 1]))
   
 
   var fullAddresses = []
 
   for (var i = 0; i < addressLines.length; i++) {
     // If current line has zipcode, it's a single line and will be pushed
-    if ((/\b\d{5}\b/g).test(addressLines[i])) {
+    if (checkForZipcode(addressLines[i])) {
       fullAddresses.push(addressLines[i])
     } else {
+
+    // If the current line doesn't have a zipcode, combine it with the next line, push it, and increment i so the next line is skipped
+    // The rationale is that the next line (i + 1) is already accounted for here. With my data shape, I can assume i + 1 will contain a zipcode
       fullAddresses.push(addressLines[i] + addressLines[i + 1])
       i++
     }
   }
 
   console.log(fullAddresses)
-  fs.writeFileSync(`./addresses-aa-${8}.txt`, fullAddresses.join('\n'));
+
+  // fs.writeFileSync(`./addresses-aa-${8}.txt`, fullAddresses.join('\n'));
 });
 
 parseFile(`data/aa-${8}.txt`)
---
+++ week-3/app.js
@@ -0,0 +1,96 @@
+"use strict"
+
+// dependencies
+const fs = require('fs'),
+      querystring = require('querystring'),
+      got = require('got'),
+      async = require('async'),
+      dotenv = require('dotenv');
+
+// TAMU api key
+dotenv.config();
+const API_KEY = process.env.TAMU_KEY;
+const API_URL = 'https://geoservices.tamu.edu/Services/Geocode/WebService/GeocoderWebServiceHttpNonParsed_V04_01.aspx'
+
+// geocode addresses
+let meetingsData = [];
+let addresses = [
+  '109 West 129th Street',
+  '240 West 145th Street',
+  '469 West 142nd Street',
+  '204 West 134th Street',
+  '2044 Adam Clayton Powell Blvd.',
+  '469 West 142nd Street',
+  '521 W 126th St',
+  '109 West 129th Street',
+  '469 West 142nd Street',
+  '2044 Seventh Avenue',
+  '127 West 127th Street',
+  '310 West 139th Street',
+  '409 West 141st Street',
+  '91 Claremont Avenue',
+  '1727 Amsterdam Avenue',
+  '469 West 142nd Street',
+  '91 Claremont Avenue',
+  '219 West 132nd Street',
+  '211 West 129th Street',
+  '425 West 144th Street',
+  '204 West 134th Street',
+  '506 Lenox Avenue',
+  '1727 Amsterdam Avenue',
+  '1854 Amsterdam Avenue',
+  '469 West 142nd Street',
+  '58-66 West 135th Street',
+]
+
+// eachSeries in the async module iterates over an array and operates on each item in the array in series
+async.eachSeries(addresses, function(value, callback) {
+    let query = {
+        streetAddress: value,
+        city: "New York",
+        state: "NY",
+        apikey: API_KEY,
+        format: "json",
+        version: "4.01"
+    };
+
+    // construct a querystring from the `query` object's values and append it to the api URL
+    let apiRequest = API_URL + '?' + querystring.stringify(query);
+
+    (async () => {
+    	try {
+        console.log('API', apiRequest)
+    		const response = await got(apiRequest);
+        var resBody = JSON.parse(response.body)
+        console.log('RES RES', resBody)
+
+    		// let tamuGeo = JSON.parse(response.body);
+        // [ 
+        //   { address: '63 Fifth Ave, New York, NY', latLong: { lat: 40.7353041, lng: -73.99413539999999 } },
+        //   { address: '16 E 16th St, New York, NY', latLong: { lat: 40.736765, lng: -73.9919024 } },
+        //   { address: '2 W 13th St, New York, NY', latLong: { lat: 40.7353297, lng: -73.99447889999999 } } 
+        // ]
+        let obj = {
+          address: value + ', New York, NY',
+          latLong: {
+            lat: resBody.OutputGeocodes[0].OutputGeocode.Latitude,
+            long: resBody.OutputGeocodes[0].OutputGeocode.Longitude
+          }
+          // response.body.OutputGeocodes[0].OutputGeocode
+        }
+    		// console.log(tamuGeo['FeatureMatchingResultType'], apiRequest);
+    		meetingsData.push(obj);
+    	} catch (error) {
+    		console.log('ERROOORRRR', error.response.body);
+    	}
+    })();
+
+    // sleep for a couple seconds before making the next request
+    setTimeout(callback, 2000);
+}, function() {
+    // JSON.stringify(meetingsData)
+    console.log(meetingsData)
+    console.log('*** *** *** *** ***');
+    console.log(`Number of meetings in this zone: ${meetingsData.length}`);
+    fs.writeFileSync('./geocoded-addresses.txt', JSON.stringify(meetingsData))
+});
